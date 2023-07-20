20661fd38f404d28cfb71804e1a931c462eaf170
hericfelix <84871186+hericfelix@users.noreply.github.com>
2023-07-20 02:09:18 -0300

    feat: added filters to home page

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -4,21 +4,58 @@ import { ProductCard } from '@components/product-card';
 
 import { useProducts } from 'src/contexts';
 
-import { ProductsList } from './styles';
+import {
+  ProductsList,
+  PageTitle,
+  TitleContainer,
+  HomeProductsContentContainer,
+  MainContainer
+} from './styles';
+import { MobileFilter } from './components/filter/mobile-filter';
+import { Skeleton } from '@chakra-ui/react';
+import { useDeviceType } from 'src/hooks';
+import { DesktopFilter } from './components/filter/desktop-filter';
 
 export function HomeTemplate() {
   const { allProductsQuery } = useProducts();
 
+  const deviceType = useDeviceType();
+
   const productsNodes = allProductsQuery.data?.allProductsJson.edges;
 
-  console.log(productsNodes);
+  function handleResultsText() {
+    const productsCount = allProductsQuery.data?.allProductsJson.totalCount;
+
+    if (productsCount === 0) {
+      return 'Nenhum produto encontrado';
+    }
+    if (productsCount === 1) {
+      return `1 produto encontrado`;
+    }
+
+    return `${productsCount} produtos encontrados`;
+  }
 
   return (
-    <div>
-      Olá!
-      <ProductsList>
-        {productsNodes?.map((node) => <ProductCard product={node.node} />)}
-      </ProductsList>
-    </div>
+    <MainContainer>
+      <TitleContainer>
+        <PageTitle>Productos de higiene personal</PageTitle>
+
+        {deviceType !== 'desktop' && <MobileFilter />}
+        <Skeleton isLoaded={!allProductsQuery.fetching}>
+          <p className="results_total">{handleResultsText()}</p>
+        </Skeleton>
+      </TitleContainer>
+      <HomeProductsContentContainer>
+        {deviceType === 'desktop' && <DesktopFilter />}
+        <ProductsList>
+          {allProductsQuery.fetching &&
+            Array.from({ length: 8 }).map((_, index) => (
+              <Skeleton key={index} width="auto" height="26.25rem" />
+            ))}
+          {productsNodes?.map((node) => <ProductCard product={node.node} />)}
+        </ProductsList>
+      </HomeProductsContentContainer>
+    </MainContainer>
   );
 }
--- src/templates/home/styles.ts
+++ src/templates/home/styles.ts
@@ -1,13 +1,60 @@
 import { styled } from 'styled-components';
 
+export const MainContainer = styled.main`
+  padding: 1rem 1.5rem;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    padding-inline: 2.5rem;
+  }
+`;
+
 export const ProductsList = styled.ul`
   display: grid;
   grid-template-columns: 1fr;
   gap: 1.25rem;
 
-  padding: 1rem;
+  width: 100%;
 
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.md}) {
-    grid-template-columns: repeat(auto-fit, minmax(17.5rem, 1fr));
+    grid-template-columns: repeat(12, 1fr);
   }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.xl}) {
+    grid-template-columns: repeat(16, 1fr);
+  }
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.xxl}) {
+    grid-template-columns: repeat(20, 1fr);
+  }
+`;
+
+export const PageTitle = styled.h1`
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.neutral.neutral6};
+  font-size: ${({ theme }) => theme.fontSizes.md};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+  }
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: center;
+
+  padding: 1rem;
+
+  .results_total {
+    font-weight: 600;
+    font-size: ${({ theme }) => theme.fontSizes.md};
+    color: ${({ theme }) => theme.colors.neutral.neutral5};
+    text-align: center;
+  }
+`;
+
+export const HomeProductsContentContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  width: 100%;
 `;
